30dbb2393f71559d343b18ea2712106810f29ecf
Alex Smith <Alex@Alexs-MacBook-Pro-3.local>
2015-06-28 15:04:11 +1000

    About to implement state restoration

--- GameHouse/AppDelegate.m
+++ GameHouse/AppDelegate.m
@@ -26,4 +26,15 @@
     [self.window makeKeyAndVisible];
     return YES;
 }
+
+-(BOOL)application:(UIApplication *)application shouldSaveApplicationState:(NSCoder *)coder
+{
+    return YES;
+}
+
+-(BOOL)application:(UIApplication *)application shouldRestoreApplicationState:(NSCoder *)coder
+{
+    return YES;
+}
+
 @end
